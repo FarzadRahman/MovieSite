06bf95d71194cce0cfede069e658f6c1825ee16b
Farzad <farzadrahman59@gmail.com>
2019-03-27 13:22:42 +0600

    Farzad

---
+++ resources/views/header.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from codepassenger.com/html/bigshow/celebrities.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 26 Mar 2019 11:39:01 GMT -->
+<head>
+    <!-- Site information -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BigShow - Movie / Film / Cinema Template</title>
+    <meta name="description" content="Bigshow is a template to jump start campaigning your various kind of movie or tv show">
+
+    <!-- External CSS -->
+    <link rel="stylesheet" href="{{url('public')}}/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{url('public')}}/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{url('public')}}/assets/css/owl.carousel.css">
+    <link rel="stylesheet" href="{{url('public')}}/assets/css/owl.transitions.css">
+    <link rel="stylesheet" href="{{url('public')}}/assets/css/magnific-popup.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{url('public')}}/assets/css/style.css">
+    <link rel="stylesheet" href="{{url('public')}}/assets/css/responsive.css">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{url('public')}}/images/template/favicon.png">
+    <link rel="apple-touch-icon" href="{{url('public')}}/images/template/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{url('public')}}/images/template/icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{url('public')}}/images/template/icon-114x114.png">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="{{url('public')}}/assets/js/html5shiv.min.js"></script>
+      <script src="{{url('public')}}/assets/js/respond.min.js"></script>
+
+
+    <![endif]-->
+
+
+</head>
+
+<body>
+
+   
+
+    <!-- Top Header -->
+   <!--  <header class="topbar text-white" id="topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-8">
+                    <p class="topbar-intro">Enjoy your favourite movie, TV show &amp; video in a single subscription at $60</p>
+                </div>
+                <div class="col-lg-6 col-sm-4">
+                    <div class="topbar-right-btns">
+                        <a class="btn" href="login.html">Log In</a>
+                        <a class="btn" href="signup.html">Register</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header> -->
+    <!-- Top Header End -->
+    @include('navbar')
+
+
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,250 @@
+@extends('main')
+@section('content')
+
+
+
+
+    <!-- Banne Slider -->
+    <div class="banner-slider-area text-white">
+        <div id="banner-slider" class="owl-carousel banner-slider">
+            <div class="banner-item" style='background-image: url("{{url('public/images/slider/1.png')}}")'>
+                <div class="overlay-70">
+                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                    <div class="banner-content">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6">
+                                    <span class="rating">9.5</span>
+                                    <h1 class="banner-title"><a href="movie-detail.html">Transformers: The Last Knight</a></h1>
+                                    <p class="banner-date-meta">20 Jun, 2017</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="banner-item" style='background-image: url("{{url('public/images/slider/2.png')}}")'>
+                <div class="overlay-70">
+                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                    <div class="banner-content">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6">
+                                    <span class="rating">9.5</span>
+                                    <h1 class="banner-title"><a href="movie-detail.html">Transformers: The Last Knight</a></h1>
+                                    <p class="banner-date-meta">20 Jun, 2017</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="banner-item" style='background-image: url("{{url('public/images/slider/3.png')}}")'>
+                <div class="overlay-70">
+                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                    <div class="banner-content">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-6">
+                                    <span class="rating">9.5</span>
+                                    <h1 class="banner-title"><a href="movie-detail.html">Transformers: The Last Knight</a></h1>
+                                    <p class="banner-date-meta">20 Jun, 2017</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Banne Slider End -->
+
+
+
+    <div class="main-wrap">
+
+        <!-- Movie Section -->
+        <div class="section section-padding movie-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 col-xs-6">
+                        <div class="section-header">
+                            <h2 class="section-title">Latest Movies</h2>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-xs-6">
+                        <a class="all-link" href="movie.html">See All Movies</a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="owl-carousel video-carousel" id="video-carousel">
+                        <div class="video-item">
+                            <div class="thumb-wrap">
+                                <img src="{{url('public')}}/images/movies/1.png" alt="Movie Thumb">
+                                <span class="rating">9.2</span>
+                                <div class="thumb-hover">
+                                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                                </div>
+                            </div>
+                            <div class="video-details">
+                                <h4 class="video-title"><a href="movie-detail.html">Wonder Women</a></h4>
+                                <p class="video-release-on">22 Jun 2017</p>
+                            </div>
+                        </div>
+                        <div class="video-item">
+                            <div class="thumb-wrap">
+                                <img src="{{url('public')}}/images/movies/2.png" alt="Movie Thumb">
+                                <span class="rating">9.2</span>
+                                <div class="thumb-hover">
+                                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                                </div>
+                            </div>
+                            <div class="video-details">
+                                <h4 class="video-title"><a href="movie-detail.html">Captain America</a></h4>
+                                <p class="video-release-on">22 Jun 2017</p>
+                            </div>
+                        </div>
+                        <div class="video-item">
+                            <div class="thumb-wrap">
+                                <img src="{{url('public')}}/images/movies/3.png" alt="Movie Thumb">
+                                <span class="rating">9.2</span>
+                                <div class="thumb-hover">
+                                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                                </div>
+                            </div>
+                            <div class="video-details">
+                                <h4 class="video-title"><a href="movie-detail.html">The City Truck</a></h4>
+                                <p class="video-release-on">22 Jun 2017</p>
+                            </div>
+                        </div>
+                        <div class="video-item">
+                            <div class="thumb-wrap">
+                                <img src="{{url('public')}}/images/movies/4.png" alt="Movie Thumb">
+                                <span class="rating">9.2</span>
+                                <div class="thumb-hover">
+                                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                                </div>
+                            </div>
+                            <div class="video-details">
+                                <h4 class="video-title"><a href="movie-detail.html">Dark Night 2</a></h4>
+                                <p class="video-release-on">22 Jun 2017</p>
+                            </div>
+                        </div>
+                        <div class="video-item">
+                            <div class="thumb-wrap">
+                                <img src="{{url('public')}}/images/movies/1.png" alt="Movie Thumb">
+                                <span class="rating">9.2</span>
+                                <div class="thumb-hover">
+                                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                                </div>
+                            </div>
+                            <div class="video-details">
+                                <h4 class="video-title"><a href="movie-detail.html">Wonder Women</a></h4>
+                                <p class="video-release-on">22 Jun 2017</p>
+                            </div>
+                        </div>
+                        <div class="video-item">
+                            <div class="thumb-wrap">
+                                <img src="{{url('public')}}/images/movies/2.png" alt="Movie Thumb">
+                                <span class="rating">9.2</span>
+                                <div class="thumb-hover">
+                                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                                </div>
+                            </div>
+                            <div class="video-details">
+                                <h4 class="video-title"><a href="movie-detail.html">Captain America</a></h4>
+                                <p class="video-release-on">22 Jun 2017</p>
+                            </div>
+                        </div>
+                        <div class="video-item">
+                            <div class="thumb-wrap">
+                                <img src="{{url('public')}}/images/movies/3.png" alt="Movie Thumb">
+                                <span class="rating">9.2</span>
+                                <div class="thumb-hover">
+                                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                                </div>
+                            </div>
+                            <div class="video-details">
+                                <h4 class="video-title"><a href="movie-detail.html">The City Truck</a></h4>
+                                <p class="video-release-on">22 Jun 2017</p>
+                            </div>
+                        </div>
+                        <div class="video-item">
+                            <div class="thumb-wrap">
+                                <img src="{{url('public')}}/images/movies/4.png" alt="Movie Thumb">
+                                <span class="rating">9.2</span>
+                                <div class="thumb-hover">
+                                    <a class="play-video" href="https://www.youtube.com/watch?v=5cY5PHE4x_g"><i class="fa fa-play"></i></a>
+                                </div>
+                            </div>
+                            <div class="video-details">
+                                <h4 class="video-title"><a href="movie-detail.html">Dark Night 2</a></h4>
+                                <p class="video-release-on">22 Jun 2017</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Movie Section End -->
+
+
+
+ 
+
+        <!-- News -->
+        <div class="section section-padding top-padding-normal news-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="section-header">
+                            <h2 class="section-title">News Feed</h2>
+                        </div>
+                    </div>
+                    <div class="col-xs-4">
+                        <a class="all-link" href="news.html">See All News</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <a class="news-featured" href="news-detail.html">
+                            <img class="img-responsive" src="images/news/featured-news.png" alt="Featured news thumb">
+                            <span class="news-date-meta">12 Jun, 2017</span>
+                            <div class="news-content">
+                                <h4 class="news-title">It is a long established fact that a reader will be distracted by the readable</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6 sm-top-30">
+                        <div class="news-item">
+                            <a class="news-thumb" href="news-detail.html">
+                                <img src="images/news/1.png" alt="News thumb">
+                            </a>
+                            <div class="news-content">
+                                <span class="news-date-meta">25 Jun, 2017</span>
+                                <h4 class="news-title"><a href="news-detail.html">It is a long established fact that</a></h4>
+                                <p class="news-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis sequi ut recusandae, vero voluptate expedita unde.</p>
+                            </div>
+                        </div>
+                        <div class="news-item">
+                            <a class="news-thumb" href="news-detail.html">
+                                <img src="images/news/2.png" alt="News thumb">
+                            </a>
+                            <div class="news-content">
+                                <span class="news-date-meta">25 Jun, 2017</span>
+                                <h4 class="news-title"><a href="news-detail.html">It is a long established fact that</a></h4>
+                                <p class="news-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis sequi ut recusandae, vero voluptate expedita unde.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- News End -->
+
+    </div>
+
+
+
+@endsection
